e8956f5fc6c35be6b1b571ee89d406593628de22
bmatteo <mateusz.bereda@student.uj.edu.pl>
2018-10-14 11:02:17 +0200

    Init

---
+++ src/main/java/com/bereda/mateusz/Main.java
@@ -0,0 +1,7 @@
+package com.bereda.mateusz;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/bereda/mateusz/model/CustomerB2B.java
@@ -0,0 +1,49 @@
+package com.bereda.mateusz.model;
+
+public class CustomerB2B {
+    private int id;
+    private String companyName;
+    private String nip;
+    private String regon;
+    private String fax;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+
+    public String getRegon() {
+        return regon;
+    }
+
+    public void setRegon(String regon) {
+        this.regon = regon;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+}
